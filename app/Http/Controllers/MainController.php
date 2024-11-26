4030cb0046cfd6fbb70f3e9578fefaefdb11c1cf
dossinaraceh <developeronsystem@gmail.com>
2024-11-26 16:50:30 +0700

    adding perusahaan table

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,6 +6,7 @@ use App\Models\About;
 use App\Models\Testimoni;
 use App\Models\Distribution;
 use App\Models\Brand;
+use App\Models\Perusahaan;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
@@ -17,9 +18,10 @@ class MainController extends Controller
         $about = About::all();
         $testimoni = Testimoni::all();
         $distribution = Distribution::all();
+        $perusahaan = Perusahaan::all();
         $brand = Brand::all();
     
-        return view("beranda", compact('slide', 'about', 'testimoni', 'distribution', 'brand'));
+        return view("beranda", compact('slide', 'about', 'testimoni', 'distribution', 'brand', 'perusahaan'));
     }
     
     
---
+++ app/Http/Controllers/PerusahaanController.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Perusahaan;
+use Illuminate\Http\Request;
+
+class PerusahaanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $perusahaan = Perusahaan::all();
+        return view('perusahaan.perusahaan', compact('perusahaan'));
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('perusahaan.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'nmperusahaan' => 'required',
+            'alamat' => 'required',
+            'telp' => 'required',
+            'email' => 'required|email',
+            'descsingkat' => 'required',
+        ]);
+        
+        $perusahaan = new Perusahaan();
+        $perusahaan->nmperusahaan = $validatedData['nmperusahaan'];
+        $perusahaan->alamat = $validatedData['alamat'];
+        $perusahaan->telp = $validatedData['telp'];
+        $perusahaan->email = $validatedData['email'];
+        $perusahaan->descsingkat = $validatedData['descsingkat'];
+            
+        $perusahaan->save();
+
+        return redirect('/dashboard/perusahaan');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $perusahaan = Perusahaan::find($id);
+        return view('perusahaan.edit', compact('perusahaan'));
+        return view('perusahaan.edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+
+        $validatedData = $request->validate([
+            'nmperusahaan' => 'required',
+            'alamat' => 'required',
+            'telp' => 'required',
+            'email' => 'required|email',
+            'descsingkat' => 'required',
+        ]);
+        
+        $perusahaan = Perusahaan::findOrFail($id);
+        $perusahaan->nmperusahaan = $validatedData['nmperusahaan'];
+        $perusahaan->alamat = $validatedData['alamat'];
+        $perusahaan->telp = $validatedData['telp'];
+        $perusahaan->email = $validatedData['email'];
+        $perusahaan->descsingkat = $validatedData['descsingkat'];
+            
+        $perusahaan->save();
+
+        return redirect('/dashboard/perusahaan');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $perusahaan = Perusahaan::findOrFail($id);
+        $perusahaan->delete();
+        return redirect('/dashboard/perusahaan');
+    }
+}
